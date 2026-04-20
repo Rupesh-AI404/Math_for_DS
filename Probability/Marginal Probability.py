f2bad5ca82615120935de99861e6564ec4a795b5
Rupesh <rupeshghimire377@gmail.com>
2026-04-20 23:09:18 +0545

    Marginal Probability

---
+++ Probability/Marginal Probability.py
@@ -0,0 +1,11 @@
+# Out of 1000 emails: 200 are spam
+
+# Marginal probability = The chance of something happening by itself, ignoring everything else.
+
+
+total   = 1000
+spam    = 200
+
+P_spam  = spam / total
+print(f"P(spam)     = {P_spam}")      # 0.2  → 20% chance any email is spam
+print(f"P(not spam) = {1 - P_spam}")  # 0.8  → always sums to 1.0
